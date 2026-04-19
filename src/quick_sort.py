1319ec6cb3e8e441ff77e2ab9f2e79e8435ebf4c
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2026-04-19 17:37:16 -0300

    Initial commit

---
+++ src/quick_sort.py
@@ -0,0 +1,11 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivo = arr[len(arr) // 2]
+
+    menores = [x for x in arr if x < pivo]
+    iguais  = [x for x in arr if x == pivo]
+    maiores = [x for x in arr if x > pivo]
+
+    return quick_sort(menores) + iguais + quick_sort(maiores)
